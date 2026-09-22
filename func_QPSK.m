70ff9e816abdd9a9b9b743274670b63676891fe2
happyending111 <1944828083@qq.com>
2026-09-22 12:04:37 +0800

    Delete func_QPSK.m

--- func_QPSK.m
+++
@@ -1,4 +0,0 @@
-function Dmap_qpsk=func_QPSK(data,LEN_data,LEN_frame)
-D_qpsk   = comm.QPSKModulator('BitInput',false);
-Dmap_qpsk= D_qpsk(data);
-Dmap_qpsk= reshape(Dmap_qpsk,LEN_data,LEN_frame).';
