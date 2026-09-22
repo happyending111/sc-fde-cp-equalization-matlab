b6dc65cf54bfbd3647d83f388edb48cda70da50b
happyending111 <1944828083@qq.com>
2026-09-22 12:04:59 +0800

    Delete func_add_fre_phase_offset.m

--- func_add_fre_phase_offset.m
+++
@@ -1,4 +0,0 @@
-function r=func_add_fre_phase_offset(Rdata_dw,LEN_ALL,Fre_offset,Phase_offset);
-n                   = 0:1:length(Rdata_dw)-1;
-%添加频偏相偏
-r                   = Rdata_dw.*exp(1j*2*pi*Fre_offset*n/LEN_ALL+1j*Phase_offset); 
