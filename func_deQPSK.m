2044ff7ac9a4f838c2c0bce4fe4ee049d5041b53
happyending111 <1944828083@qq.com>
2026-09-22 12:05:24 +0800

    Delete func_deQPSK.m

--- func_deQPSK.m
+++
@@ -1,6 +0,0 @@
-function [rxData1,rxData]=func_deQPSK(tmps1,tmps2);
-tmps1           = reshape(tmps1.',1,size(tmps1,1)*size(tmps1,2));
-tmps2           = reshape(tmps2.',1,size(tmps2,1)*size(tmps2,2));
-qpskDemodulator = comm.QPSKDemodulator('BitOutput',false);
-rxData          = qpskDemodulator(tmps1.');
-rxData1         = qpskDemodulator(tmps2.');
