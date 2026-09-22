738bc93f26446edc56ea7f832a2241b5885371de
happyending111 <1944828083@qq.com>
2026-09-22 12:05:13 +0800

    Delete func_channel.m

--- func_channel.m
+++
@@ -1,9 +0,0 @@
-function Rdata=func_channel(Tdatas);
-
-ricianchan = comm.RicianChannel(...
-    'SampleRate',1e6,...                %采样率
-    'PathDelays',[0.0 0.5 1.2]*1e-6,... %延迟
-    'AveragePathGains',[0.1 0.5 0.2],...%增益
-    'KFactor',7,...                     %银子
-    'DirectPathDopplerShift',0.6);      %频移
-Rdata=ricianchan(Tdatas.').';
